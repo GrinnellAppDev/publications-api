d6af8175b6c05dacce515914a872502d1729a668
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2016-12-04 13:45:40 -0600

    Switch to serverless workflow

--- articles.py
+++ articles.py
@@ -20,21 +20,21 @@ def create_response(status_code, headers={}, res=None, err=None):
         "statusCode": str(status_code),
         "body": (
             err.message if err is not None
-            else json.dumps(res) if res is not None 
+            else json.dumps(res) if res is not None
             else ""
         ),
         "headers": resp_headers
     }
-    
 
-def lambda_handler(event, context):
+
+def post(event, context):
     """
     Handle a post request.
-    
+
     Validate the content, create a UUID, and then write to the database.
     """
     assert event["httpMethod"] == "POST"
-    
+
     db = boto3.resource("dynamodb")
     articles_table = db.Table("Articles")
     publications_table = db.Table("Publications")
@@ -42,14 +42,14 @@ def lambda_handler(event, context):
     item = json.loads(event["body"])
     path_params = event["pathParameters"]
     publication_id = path_params["publicationId"]
-    
+
     # todo: validate the item with to a schema
-    
+
     publication_get = publications_table.get_item(
         Key={"id": publication_id},
         ProjectionExpression="id"
     )
-    
+
     if "Item" not in publication_get:
         err = Exception("No publication with id: {}".format(publication_id))
         return create_response(404, err=err)
@@ -62,4 +62,3 @@ def lambda_handler(event, context):
     articles_table.put_item(Item=item)
 
     return create_response(204, {"x-id": item_id})
-
