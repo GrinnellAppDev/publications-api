35857484e86fceb94c649e50b41057e517763c44
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2016-12-04 13:56:20 -0600

    Format validate.py

--- validate.py
+++ validate.py
@@ -7,21 +7,25 @@ import boto3
 
 
 class InvalidPublicationError(Exception):
-  def __init__(self,publication_id):
-    super(InvalidPublicationError,self).__init__("No publication with id: {}".format(publication_id))
+
+    def __init__(self, publication_id):
+        super(InvalidPublicationError, self).__init__(
+            "No publication with id: {}".format(publication_id))
+
 
 def validate_publication_id():
-  publication_get = publications_table.get_item(
-    Key={"id": publication_id},
-    ProjectionExpression="id"
-  )
-  if "Item" not in publication_get:
-    raise InvalidPublicationError
+    publication_get = publications_table.get_item(
+        Key={"id": publication_id},
+        ProjectionExpression="id"
+    )
+    if "Item" not in publication_get:
+        raise InvalidPublicationError
+
 
 def validate_request(event, context):
-  """
-  Handle a post request.
+    """
+    Handle a post request.
 
-  Validate the content, create a UUID, and then write to the database.
-  """
-  pass
+    Validate the content, create a UUID, and then write to the database.
+    """
+    pass
