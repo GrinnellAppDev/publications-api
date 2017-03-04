3da4f105e23d27141e1e259a2a5c97396211b83e
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-03-04 02:24:57 -0600

    Add ArticleCreate model

--- resources/models/ArticleEdit.js
+++ resources/models/ArticleEdit.js
@@ -3,22 +3,5 @@ module.exports.schema = () => ({
     title: "ArticleEdit",
     description: "The mutable fields of an article",
     additionalProperties: false,
-    properties: {
-        headerImage: Object.assign(require("./URL").schema(), {
-            description: "The URL of the header image",
-        }),
-        title: {
-            type: "string",
-            description: "The headline of the article",
-        },
-        content: {
-            type: "string",
-            description: "The full text of the article formatted with markdown",
-        },
-        authors: {
-            type: "array",
-            description: "A list of all collaborators on the article",
-            items: require("./Author").schema(),
-        },
-    },
+    properties: require("./ArticleCreate").schema().properties,
 });
