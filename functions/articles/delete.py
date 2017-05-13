3b9e4585669a52a0597e881c4d07ce07a0ff45aa
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-05-12 23:10:28 -0500

    Fix article delete

--- functions/articles/delete.py
+++ functions/articles/delete.py
@@ -33,11 +33,11 @@ def handler(event, context, db):
     validate_publication_id(publication_id, db)
 
     article_key = {"publication": publication_id, "id": article_id}
-    articles_get = db.articles.get(article_key)
+    article = db.articles.get(article_key)
 
-    if articles_get is None:
+    if article is None:
         raise InvalidArticleError(article_id)
 
     db.articles.delete(article_key)
 
-    return create_json_response(200, body=articles_get["Item"])
+    return create_json_response(200, body=article)
