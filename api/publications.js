933085a406ae44c5c295784b8a6058a8f12f6702
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-02-25 15:18:26 -0600

    Add article listing

--- api/publications.js
+++ api/publications.js
@@ -5,7 +5,9 @@ const {
     runWithDB,
     validateRequest,
     base64ToId,
+    base64ToNumber,
     idToBase64,
+    numberToBase64,
     HTTPError
 } = require("./util")
 
@@ -104,44 +106,132 @@ module.exports = Router()
         })
     )
 
-/**
- * @swagger
- *  /publications/{publicationId}/articles:
- *    get:
- *      summary: Get a list of article thumbnails in a particular publication
- *      description:
- *      parameters:
- *        - name: pageSize
- *          in: query
- *          description: Maximum number of items per response
- *          schema:
- *            type: integer
- *            default: 10
- *        - name: pageToken
- *          in: query
- *          description: Token representing a particular page of results
- *          schema:
- *            type: string
- *      responses:
- *        200:
- *          description: A list of articles
- *          headers:
- *            Link:
- *              schema:
- *                type: string
- *              description: >
- *                Standard HTTP Link header. All URIs relative to the /publications endpoint.
- *          content:
- *            application/json:
- *              schema:
- *                type: array
- *                items:
- *                  $ref: "#/components/schemas/ArticleThumbnail"
- *        400:
- *          $ref: "#/components/responses/BadRequest"
- *        404:
- *          $ref: "#/components/responses/NotFound"
- */
+    /**
+     * @swagger
+     *  /publications/{publicationId}/articles:
+     *    get:
+     *      summary: Get a list of article thumbnails in a particular publication
+     *      description:
+     *      parameters:
+     *        - name: publicationId
+     *          in: path
+     *          required: true
+     *          description: The publication to query
+     *          schema:
+     *            type: string
+     *        - name: pageSize
+     *          in: query
+     *          description: Maximum number of items per response
+     *          schema:
+     *            type: integer
+     *            default: 10
+     *        - name: pageToken
+     *          in: query
+     *          description: Token representing a particular page of results
+     *          schema:
+     *            type: string
+     *      responses:
+     *        200:
+     *          description: A list of articles
+     *          headers:
+     *            Link:
+     *              schema:
+     *                type: string
+     *              description: >
+     *                Standard HTTP Link header. All URIs relative to the /publications endpoint.
+     *          content:
+     *            application/json:
+     *              schema:
+     *                type: array
+     *                items:
+     *                  $ref: "#/components/schemas/ArticleThumbnail"
+     *        400:
+     *          $ref: "#/components/responses/BadRequest"
+     *        404:
+     *          $ref: "#/components/responses/NotFound"
+     */
+    .get("/:publicationId/articles", (request, response) =>
+        runWithDB(async db => {
+            validateRequest(request, {
+                paramSchemaProps: {
+                    publicationId: { type: "string" }
+                },
+                querySchemaProps: {
+                    pageSize: { type: "string", pattern: "^\\d+$" },
+                    pageToken: { type: "string", format: "urlsafeBase64" }
+                }
+            })
+
+            const publicationsCollection = db.collection("publications")
+            const articlesCollection = db.collection("articles")
+
+            /** @type {number} */ const pageSize = +request.query.pageSize || 10
+            /** @type {string} */ const pageToken =
+                request.query.pageToken || null
+            /** @type {string} */ const publicationId =
+                request.params.publicationId
+
+            let pageTokenValue = 0
+            if (pageToken) {
+                try {
+                    pageTokenValue = base64ToNumber(pageToken)
+                } catch (error) {
+                    throw new HTTPError(400, "Invalid pageToken.")
+                }
+            }
+
+            if (
+                !await publicationsCollection
+                    .find({
+                        _id: publicationId
+                    })
+                    .hasNext()
+            ) {
+                throw new HTTPError(
+                    404,
+                    `No publication with id "${publicationId}".`
+                )
+            }
+
+            const query = { publication: publicationId }
+            const articles = pageToken
+                ? articlesCollection.find({
+                      ...query,
+                      datePublished: { $lte: pageTokenValue }
+                  })
+                : articlesCollection.find(query)
+
+            const limitedArticles = await articles
+                .sort("datePublished", -1)
+                .limit(pageSize + 1)
+                .toArray()
+
+            const items = limitedArticles.slice(0, pageSize).map(article => ({
+                id: article.id,
+                publication: article.publication,
+                title: article.title,
+                datePublished: article.datePublished,
+                authors: article.authors,
+                readTimeMinutes: article.readTimeMinutes,
+                headerImage: article.headerImage
+            }))
+
+            const nextPageFirstArticle = limitedArticles[pageSize]
+            if (nextPageFirstArticle) {
+                response.links({
+                    next: `articles?${querystring.stringify({
+                        ...request.query,
+                        pageToken: numberToBase64(
+                            nextPageFirstArticle.datePublished
+                            // TODO: mash in the id for unique ordering
+                        )
+                    })}`
+                })
+            }
+
+            response.status(200).send(items)
+        })
+    )
 
 /**
  * @swagger
