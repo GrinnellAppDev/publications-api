3cee69c288fdfe54d7f5e0f902d49f76544de292
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-03-04 15:15:31 -0600

    Update docs

---
+++ insert-sample-articles.js
@@ -0,0 +1,65 @@
+;[
+  {
+    readTimeMinutes: 0.02,
+    title: "Foo",
+    datePublished: 1494640179478,
+    content: "Foo bar baz.  Spinning is a good trick. Bar",
+    authors: [{ name: "Bar", email: "b@a.z" }],
+    id: "foo",
+    publication: "s-and-b"
+  },
+  {
+    readTimeMinutes: 0.04,
+    title: "Wilson cutting Appdev funding",
+    datePublished: 1490935589714,
+    content:
+      'Appdev has recently come under fire!  Following a recent "State of SHACS" presentation, some students were displeased.',
+    authors: [{ name: "Solomon Grundy", email: "grundyso@grinnell.edu" }],
+    id: "wilson-cutting-appdev-funding",
+    publication: "s-and-b"
+  },
+  {
+    publication: "s-and-b",
+    title: "Wilson cutting Appdev funding",
+    url:
+      "http://www.thesandb.com/news/shacs-to-offer-funds-for-students-in-need.html",
+    datePublished: 1486373993743,
+    content:
+      "BREAKING NEWS: Appdev has recently come under fire!  It's quite a schemozzle!",
+    headerImage:
+      "http://www.thesandb.com/wp-content/uploads/2017/02/COLOR-news-divest2-contributed-1160x774.jpg",
+    authors: [{ name: "Solomon Grundy", email: "grundyso@grinnell.edu" }],
+    id: "wilson-cutting-appdev-funding2",
+    readTimeMinutes: 0.028888888888888888
+  },
+  {
+    publication: "s-and-b",
+    title: "Top 10 Tragedies the Jedi Won't Tell You!",
+    url:
+      "http://www.thesandb.com/news/shacs-to-offer-funds-for-students-in-need.html",
+    datePublished: 1486373933743,
+    content:
+      "Did you ever hear the tragedy of Darth Plagueis \"the wise\"? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. It's ironic he could save others from death, but not himself.",
+    headerImage: "https://i.ytimg.com/vi/05dT34hGRdg/maxresdefault.jpg",
+    authors: [{ name: "Sheev Palpatine", email: "palpatin@grinnell.edu" }],
+    id: "top-10-tragedies-the-jedi",
+    readTimeMinutes: 0.3088888888888889
+  },
+  {
+    publication: "s-and-b",
+    title:
+      "Best Medieval Siege Engines for Launching 90kg Over 300m.  #3 Will SHOCK You!",
+    url:
+      "http://www.thesandb.com/news/shacs-to-offer-funds-for-students-in-need.html",
+    datePublished: 1486373851046,
+    content:
+      "Here's the thing. You said a \"trebuchet is a catapult.\"\nIs it in the same family? Yes. No one's arguing that.\nAs someone who is a scientist who studies catapults, I am telling you, specifically, in science, no one calls trebuchets catapults. If you want to be \"specific\" like you said, then you shouldn't either. They're not the same thing.\nIf you're saying \"catapult family\" you're referring to the taxonomic grouping of ranged siege weapons, which includes things from mangonels to orangers to the petrary.\nSo your reasoning for calling a trebuchet a catapult is because random people \"call the flingy things ones catapults?\" Let's get rubber bands and folded pieces of paper in there, then, too.\nAlso, calling someone a human or an ape? It's not one or the other, that's not how taxonomy works. They're both. A trebuchet is a catapult and a member of the catapult family. But that's not what you said. You said a trebuchet is a catapult, which is not true unless you're okay with calling all members of the catapult family catapults, which means you'd call orangers, ballistas, and other siege weapons catapults, too. Which you said you don't.\nIt's okay to just admit you're wrong, you know?",
+    headerImage:
+      "https://s-media-cache-ak0.pinimg.com/originals/f8/4b/a9/f84ba96f639243630112d2b69cea127d.jpg",
+    authors: [
+      { name: "William the Conquerer", email: "normandy@grinnell.edu" }
+    ],
+    id: "best-medieval-siege-engines",
+    readTimeMinutes: 0.44222222222222224
+  }
+]
