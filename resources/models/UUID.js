d2bdde718461741b3de9fa219b815575f3d84893
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-03-11 15:51:49 -0500

    Delete everything

--- resources/models/UUID.js
+++
@@ -1,8 +0,0 @@
-module.exports.schema = () => ({
-    $schema: "http://json-schema.org/draft-04/schema#",
-    title: "UUID",
-    description: "A universally unique identifier string",
-    type: "string",
-    pattern: "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-" +
-             "[a-fA-F0-9]{12}$",
-});
